e65e4b9346c5c483b20ae8fcf6d5b5be0d1e4c2a
Oleks Litus <oleks.litus@gmail.com>
2021-01-30 13:30:55 -0500

    Update dig.sh

--- dig.sh
+++ dig.sh
@@ -1,12 +1,25 @@
+# 
 
 # Python 2
-python=$(python --version)
-if [[ $python == *""* ]]; then
+python=$(python --version 2>&1 | cat)
+if [[ $python == *"Python"* ]]; then
     echo $python
+else
+    echo "Python 2 - Not intalled"
 fi
 
 # Python 3
-python3=$(python3 --version)
-if [[ $python == *""* ]]; then
-    echo $python
+python3=$(python --version 2>&1 | cat)
+if [[ $python3 == *"Python"* ]]; then
+    echo $python3i
+else
+    echo "Python 3 - Not installed"
+fi
+
+# Template
+temp=$(temp --version 2>&1 | cat)
+if [[]]; then
+    echo
+else
+    echo
 fi
