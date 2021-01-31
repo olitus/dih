affaf422a36cf2d93012fc4040ec3401212fc53b
Oleks Litus <oleks.litus@gmail.com>
2021-01-30 21:21:55 -0500

    Update and rename get-dig.sh to get-pile.sh

--- get-dig.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-
-curl -o /usr/bin/dig https://github.com/chunibio/dig/blob/main/dig.sh
-chmod +x /usr/bin/dig
---
+++ get-pile.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+
+curl -o /usr/bin/pile https://github.com/chunibio/dig/blob/main/pile.sh
+chmod +x /usr/bin/pile
